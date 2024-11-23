5f66cb2da417bf435c049a682ec7dd40c1bd6a11
nemi-dev <descartmarch@gmail.com>
2024-11-23 22:54:06 +0900

    remove unused images

--- a.py
+++ a.py
@@ -1,34 +1,26 @@
+import os
+from os.path import isfile
 import csv
 from operator import itemgetter
 
-with open("./public/list.tsv", encoding="utf-8", newline="") as r:
-  l = [*csv.DictReader(r, delimiter="\t")]
+def unlink(p):
+  if isfile(p):
+    os.unlink(p)
+    print(f"{p} deleted")
 
-with open("./jack2.tsv", encoding="utf-8", newline="") as r:
-  j = [*csv.DictReader(r, delimiter="\t")]
+with open("./joined.tsv", encoding="UTF-8", newline="") as f:
+  j = [*csv.DictReader(f, delimiter="\t")]
 
-k = []
+il = [jj['ino'] for jj in j]
+il = set(il)
 
-for litem in l:
-  found = False
-  for jitem in j:
-    if litem['title'].strip() == jitem['title'].strip():
-      if jitem['ino'] == '0867': continue
-      kitem = {
-        **litem,
-        "ino": jitem["ino"],
-        "title": litem['title'].strip()
-      }
-      k.append(kitem)
-      found = True
-  
-  if not found:
-    k.append({ **litem, "ino": ""})
+imgs = os.listdir('./public/img')
+imgs.sort()
+imgs = set(map(lambda x : x[:4], imgs))
 
-headers = ['ino', 'no', 'title', 'artist', 'bpm', 'slow', 'nov', 'adv', 'exh', 'mxm', 'unov', 'uadv', 'uexh', 'umxm', 'from', 'at', 'etc']
 
-# k.sort(key = itemgetter("no"))
-with open("./joined.tsv", "w", encoding="UTF-8", newline="") as w:
-  writer = csv.DictWriter(w, headers, delimiter="\t")
-  writer.writeheader()
-  writer.writerows(k)
+for lack in imgs - il:
+  unlink(f"./public/img/{lack}-inov.jpg")
+  unlink(f"./public/img/{lack}-iadv.jpg")
+  unlink(f"./public/img/{lack}-iexh.jpg")
+  unlink(f"./public/img/{lack}-imxm.jpg")
